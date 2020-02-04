93f213c20038469b9a452f2bfddaf2d3cf0313b5
brandonricharda <brandonricharda@gmail.com>
2020-02-03 23:08:44 -0500

    Initial commit of new methodology.

--- englishNumberMaster.rb
+++ englishNumberMaster.rb
@@ -1,87 +1,33 @@
-def identifyPhrase length
-  if length == 3
-    phrase = "hundred"
-  elsif length >= 4 and length < 7
-    if length == 4 or length == 5
-      phrase = "thousand"
-    elsif length == 6
-      phrase = "hundred thousand"
-    end
-  elsif length >= 7 and length < 10
-    if length == 8 or length == 7
-      phrase = "million"
-    elsif length == 9
-      phrase = "hundred million"
-    end
-  elsif length >= 10 and length < 13
-    if length == 10 or length == 1
-      phrase = "billion"
-    elsif length == 12
-      phrase = "hundred billion"
-    end
-  elsif length >= 13
-    phrase = "trillion"
-  elsif length == 2
-    phrase = "teen"
-  elsif length == 1
-    phrase = ""
-  end
-  return phrase
-end
-
-def englishNumber number
+def englishRefactor number
+  phrase = {1 => "", 2 => "teen", 3 => "hundred", 4 => "thousand", 5 => "thousand", 6 => "hundred thousand", 7 => "million", 8 => "million", 9 => "hundred million", 10 => "billion", 11 => "billion", 12 => "hundred billion", 13 => "trillion"} 
   ones = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-  teenagers = {"one one" => "eleven", "one two" => "twelve", "one three" => "thirteen", "one four" => "fourteen", "one five" => "fifteen", "one six" => "sixteen", "one seven" => "seventeen", "one eight" => "eighteen", "one nine" => "nineteen"}
-  tens = {"one" => "ten", "two" => "twenty", "three" => "thirty", "four" => "forty", "five" => "fifty", "six" => "sixty", "seven" => "seventy", "eight" => "eighty", "nine" => "ninety"}
-  lengthArray = []
-  numberToString = number.to_s
+  tens = {"one teen zero" => "ten", "two teen" => "twenty", "three teen" => "thirty", "four teen" => "forty", "five teen" => "fifty", "six teen" => "sixty", "seven teen" => "seventy", "eight teen" => "eighty", "nine teen" => "ninety"}
+  teenagers = {"one teen one" => "eleven", "one teen two" => "twelve", "one teen three" => "thirteen", "one teen four" => "fourteen", "one teen five" => "fifteen", "one teen six" => "sixteen", "one teen seven" => "seventeen", "one teen eight" => "eighteen", "one teen nine" => "nineteen"}
   numberWords = []
-  numberIntoWords = ""
-  numberToString.split("").each do |i|
+  firstDraft = ""
+  number.to_s.split("").each do |i|
     numberWords.push(ones[i.to_i])
   end
-  numberOfNumbers = numberWords.count
   numberWords.each_with_index do |i, index|
-    remaining = numberOfNumbers - index
-    if remaining > 2 and i != "zero"
-      if remaining == 5
-        if numberWords[index + 1] == "zero"
-          numberIntoWords += tens[i] + " " + identifyPhrase(remaining) + " "
-        else
-          numberIntoWords += teenagers[i + " " + numberWords[index + 1]] + " "
-        end
-      elsif remaining == 6
-        if numberWords[index + 1] == "zero"
-          numberIntoWords += i + " " + identifyPhrase(remaining) + " "
-        else
-          numberIntoWords += i + " hundred" + " "
-        end
+    remaining = numberWords.count - index
+    firstDraft += i + " " + phrase[remaining] + " "
+  end
+  firstDraftArray = firstDraft.split(" ")
+  puts firstDraftArray
+  puts "SPLIT"
+  firstDraftArray.each_with_index do |i, index|
+    if i == "teen"
+      if firstDraftArray[index + 1] != "zero"
+        firstDraftArray[index] = tens[firstDraftArray[index - 1] + " " + i]
+        firstDraftArray.delete_at(index - 1)
       else
-        if numberOfNumbers == 5
-          arrayToAnalyze = numberIntoWords.split(" ")
-          if !teenagers.key(arrayToAnalyze.last) and i != "zero"
-            numberIntoWords += + i + " " + identifyPhrase(remaining) + " "
-          else
-            numberIntoWords += identifyPhrase(remaining) + " "
-          end
-        else
-          numberIntoWords += i + " " + identifyPhrase(remaining) + " "
-        end
-      end
-    elsif remaining == 2 and i != "zero"
-      if i == "one"
-        numberIntoWords += teenagers[i + " " + numberWords[index + 1]] + " "
-      else
-        numberIntoWords += tens[i] + " "
-      end
-    elsif remaining == 1
-      arrayToAnalyze = numberIntoWords.split(" ")
-      if !teenagers.key(arrayToAnalyze.last) and i != "zero"
-        numberIntoWords += i
+        firstDraftArray[index] = tens[firstDraftArray[index - 1] + " " + i]
+        firstDraftArray.delete_at(index + 1)
+        firstDraftArray.delete_at(index - 1)
       end
     end
   end
-  puts numberIntoWords
+  puts firstDraftArray
 end
 
-englishNumber(13333)
+englishRefactor(10000)
