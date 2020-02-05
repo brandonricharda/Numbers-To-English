a756b044938785de50745fe504e80a9a6c8bd3b4
brandonricharda <brandonricharda@gmail.com>
2020-02-04 20:35:20 -0500

    Current progress.

--- englishNumberMaster.rb
+++ englishNumberMaster.rb
@@ -1,53 +1,28 @@
 def englishRefactor number
-  phrase = {1 => "", 2 => "teen", 3 => "hundred", 4 => "thousand", 5 => "thousand", 6 => "hundred thousand", 7 => "million", 8 => "million", 9 => "hundred million", 10 => "billion", 11 => "billion", 12 => "hundred billion", 13 => "trillion"} 
-  ones = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-  tens = {"one teen" => "ten", "two teen" => "twenty", "three teen" => "thirty", "four teen" => "forty", "five teen" => "fifty", "six teen" => "sixty", "seven teen" => "seventy", "eight teen" => "eighty", "nine teen" => "ninety"}
-  teenagers = {"one teen one" => "eleven", "one teen two" => "twelve", "one teen three" => "thirteen", "one teen four" => "fourteen", "one teen five" => "fifteen", "one teen six" => "sixteen", "one teen seven" => "seventeen", "one teen eight" => "eighteen", "one teen nine" => "nineteen"}
+  phrase = {1 => "", 2 => "teen", 3 => "hundred", 4 => "thousand", 5 => "teen", 6 => "hundred-thousand", 7 => "million", 8 => "million", 9 => "hundred-million", 10 => "billion", 11 => "billion", 12 => "hundred-billion", 13 => "trillion"} 
+  numbers = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", "one teen" => "ten", "one teen one" => "eleven", "one teen two" => "twelve", "one teen three" => "thirteen", "one teen four" => "fourteen", "one teen five" => "fifteen", "one teen six" => "sixteen", "one teen seven" => "seventeen", "one teen eight" => "eighteen", "one teen nine" => "nineteen", "two teen" => "twenty", "three teen" => "thirty", "four teen" => "forty", "five teen" => "fifty", "six teen" => "sixty", "seven teen" => "seventy", "eight teen" => "eighty", "nine teen" => "ninety"}
   numberWords = []
-  firstDraft = ""
+  firstDraft = []
   number.to_s.split("").each do |i|
-    numberWords.push(ones[i.to_i])
+    numberWords.push(numbers[i.to_i])
   end
   numberWords.each_with_index do |i, index|
     remaining = numberWords.count - index
-    firstDraft += i + " " + phrase[remaining] + " "
-  end
-  firstDraftArray = firstDraft.split(" ")
-  firstDraftArray.each_with_index do |i, index|
-    if i == "teen"
-      if firstDraftArray[index + 1] != "zero"
-        firstDraftArray[index] = tens[firstDraftArray[index - 1] + " " + i]
-        firstDraftArray.delete_at(index - 1)
-      else
-        firstDraftArray[index] = tens[firstDraftArray[index - 1] + " " + i]
-        firstDraftArray.delete_at(index + 1)
-        firstDraftArray.delete_at(index - 1)
-      end
-      #we need to work through each of the phrases here, creating conditions that account for each of the duplicates in the set (i.e. 7 => million, 8 => million)
-    elsif i == "hundred"
-      #code
-    elsif i == "thousand"
-      if firstDraftArray[index + 2] == "thousand"
-        firstDraftArray[index] = "teen"
-        firstDraftArray[index] = firstDraft[index - 1] == "one" ? teenagers[firstDraftArray[index - 1] + " " + firstDraftArray[index] + " " + firstDraftArray[index + 1]] : tens[firstDraftArray[index - 1] + " " + firstDraftArray[index]]
-        firstDraftArray.delete_at(index + 1)
-        firstDraftArray.delete_at(index - 1)
-      end
-    elsif i == "hundred thousand"
-      #code
-    elsif i == "million"
-      #code
-    elsif i == "hundred million"
-      #code
-    elsif i == "billion"
-      #code
-    elsif i == "hundred billion"
-      #code
-    elsif i == "trillion"
-      #code
+    previous = numberWords[index - 1]
+    if i == "zero" and numbers.has_value?(previous)
+      whatToAdd = phrase[remaining]
+    else
+      whatToAdd = i + " " + phrase[remaining]
+    end
+    if numbers.has_key?(whatToAdd)
+      firstDraft.push(numbers[whatToAdd] + " ")
+    else
+      firstDraft.push(whatToAdd)
     end
   end
-  puts firstDraftArray
+  puts firstDraft
 end
 
-englishRefactor(1000000)
+englishRefactor(10011)
+
+#perhaps I can change the array so that a space is not added between the phrase and the number word
